4045e3582de3d245a6f4da0f7ec2b13f15181e02
JoppeD <joppe.demulder@student.kdg.be>
2016-10-21 14:23:21 +0200

    Add files via upload

---
+++ Software_Engineering_Oplossingen-master/kdg-software-engineering-yahtzee-CS/WindowsFormsApplication6/DiceUI.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yahtzee
+{
+    public partial class DiceUI : UserControl
+    {
+        private DiceController controller;
+
+        public DiceUI( DiceController controller)
+        {
+            this.controller = controller;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue1.Text = newValue.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue2.Text = newValue.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue3.Text = newValue.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue4.Text = newValue.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue5.Text = newValue.ToString();
+        }
+
+        private void buttonAll_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue1.Text = newValue.ToString();
+            newValue = this.controller.throwDice();
+            this.teerlingValue2.Text = newValue.ToString();
+            newValue = this.controller.throwDice();
+            this.teerlingValue3.Text = newValue.ToString();
+            newValue = this.controller.throwDice();
+            this.teerlingValue4.Text = newValue.ToString();
+            newValue = this.controller.throwDice();
+            this.teerlingValue5.Text = newValue.ToString();
+            this.buttonAll.Visible = false;
+            this.button1.Visible = true;
+            this.button2.Visible = true;
+            this.button3.Visible = true;
+            this.button4.Visible = true;
+            this.button5.Visible = true;
+        }
+
+        private void DiceUI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
