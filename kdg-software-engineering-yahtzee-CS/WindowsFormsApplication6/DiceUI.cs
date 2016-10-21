c78295c8690d159396389c3a7398209ed2360135
SamDCKDG <sam.decock@student.kdg.be>
2016-10-21 15:13:24 +0200

    Add files via upload

--- kdg-software-engineering-yahtzee-CS/WindowsFormsApplication6/DiceUI.cs
+++ kdg-software-engineering-yahtzee-CS/WindowsFormsApplication6/DiceUI.cs
@@ -23,8 +23,66 @@ namespace Yahtzee
         private void button1_Click(object sender, EventArgs e)
         {
             int newValue = this.controller.throwDice();
+            this.teerlingValue1.Text = newValue.ToString();
+        }
 
-            this.teerlingValue.Text = newValue.ToString();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue2.Text = newValue.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue3.Text = newValue.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue4.Text = newValue.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue5.Text = newValue.ToString();
+        }
+
+        private void buttonAll_Click(object sender, EventArgs e)
+        {
+            int newValue = this.controller.throwDice();
+            this.teerlingValue1.Text = newValue.ToString();
+            newValue = this.controller.throwDice();
+            this.teerlingValue2.Text = newValue.ToString();
+            newValue = this.controller.throwDice();
+            this.teerlingValue3.Text = newValue.ToString();
+            newValue = this.controller.throwDice();
+            this.teerlingValue4.Text = newValue.ToString();
+            newValue = this.controller.throwDice();
+            this.teerlingValue5.Text = newValue.ToString();
+            this.buttonAll.Visible = false;
+            this.button1.Visible = true;
+            this.button2.Visible = true;
+            this.button3.Visible = true;
+            this.button4.Visible = true;
+            this.button5.Visible = true;
+        }
+
+        private void DiceUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Aces_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
