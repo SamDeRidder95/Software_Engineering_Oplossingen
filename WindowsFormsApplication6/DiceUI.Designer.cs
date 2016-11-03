7425390d8729d2936745e0492515af2af624f3de
SamDeRidder95 <sam.deridder@student.kdg.be>
2016-11-03 15:50:48 +0100

    Add files via upload

---
+++ WindowsFormsApplication6/DiceUI.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Yahtzee
+{
+    partial class DiceUI
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAll = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.teerlingValue0 = new System.Windows.Forms.Label();
+            this.teerlingValue1 = new System.Windows.Forms.Label();
+            this.teerlingValue2 = new System.Windows.Forms.Label();
+            this.teerlingValue3 = new System.Windows.Forms.Label();
+            this.teerlingValue4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonAll
+            // 
+            this.buttonAll.Location = new System.Drawing.Point(4, 200);
+            this.buttonAll.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonAll.Name = "buttonAll";
+            this.buttonAll.Size = new System.Drawing.Size(270, 47);
+            this.buttonAll.TabIndex = 0;
+            this.buttonAll.Text = "werp alle teerlingen";
+            this.buttonAll.UseVisualStyleBackColor = true;
+            this.buttonAll.Click += new System.EventHandler(this.buttonAll_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(4, 134);
+            this.button0.Margin = new System.Windows.Forms.Padding(4);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(135, 47);
+            this.button0.TabIndex = 0;
+            this.button0.Text = "Bijhouden";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Visible = false;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            this.button0.BackColor = System.Drawing.Color.White;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 134);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bijhouden";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.BackColor = System.Drawing.Color.White;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(290, 134);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 47);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Bijhouden";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.BackColor = System.Drawing.Color.White;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(433, 134);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 47);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Bijhouden";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.BackColor = System.Drawing.Color.White;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(576, 134);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 47);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Bijhouden";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.BackColor = System.Drawing.Color.White;
+            // 
+            // teerlingValue0
+            // 
+            this.teerlingValue0.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teerlingValue0.Location = new System.Drawing.Point(4, 0);
+            this.teerlingValue0.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.teerlingValue0.Name = "teerlingValue0";
+            this.teerlingValue0.Size = new System.Drawing.Size(135, 130);
+            this.teerlingValue0.TabIndex = 1;
+            this.teerlingValue0.BackColor = System.Drawing.Color.White;
+            // 
+            // teerlingValue1
+            // 
+            this.teerlingValue1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teerlingValue1.Location = new System.Drawing.Point(147, 0);
+            this.teerlingValue1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.teerlingValue1.Name = "teerlingValue1";
+            this.teerlingValue1.Size = new System.Drawing.Size(135, 130);
+            this.teerlingValue1.TabIndex = 1;
+            this.teerlingValue1.BackColor = System.Drawing.Color.White;
+            // 
+            // teerlingValue2
+            // 
+            this.teerlingValue2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teerlingValue2.Location = new System.Drawing.Point(290, 0);
+            this.teerlingValue2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.teerlingValue2.Name = "teerlingValue2";
+            this.teerlingValue2.Size = new System.Drawing.Size(135, 130);
+            this.teerlingValue2.TabIndex = 1;
+            this.teerlingValue2.BackColor = System.Drawing.Color.White;
+            // 
+            // teerlingValue3
+            // 
+            this.teerlingValue3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teerlingValue3.Location = new System.Drawing.Point(433, 0);
+            this.teerlingValue3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.teerlingValue3.Name = "teerlingValue3";
+            this.teerlingValue3.Size = new System.Drawing.Size(135, 130);
+            this.teerlingValue3.TabIndex = 1;
+            this.teerlingValue3.BackColor = System.Drawing.Color.White;
+            // 
+            // teerlingValue4
+            // 
+            this.teerlingValue4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teerlingValue4.Location = new System.Drawing.Point(576, 0);
+            this.teerlingValue4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.teerlingValue4.Name = "teerlingValue4";
+            this.teerlingValue4.Size = new System.Drawing.Size(135, 130);
+            this.teerlingValue4.TabIndex = 1;
+            this.teerlingValue4.BackColor = System.Drawing.Color.White;
+            // 
+            // DiceUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.teerlingValue0);
+            this.Controls.Add(this.teerlingValue1);
+            this.Controls.Add(this.teerlingValue2);
+            this.Controls.Add(this.teerlingValue3);
+            this.Controls.Add(this.teerlingValue4);
+            this.Controls.Add(this.buttonAll);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "DiceUI";
+            this.Size = new System.Drawing.Size(738, 400);
+            this.Load += new System.EventHandler(this.DiceUI_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAll;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label teerlingValue0;
+        private System.Windows.Forms.Label teerlingValue1;
+        private System.Windows.Forms.Label teerlingValue2;
+        private System.Windows.Forms.Label teerlingValue3;
+        private System.Windows.Forms.Label teerlingValue4;
+    }
+}
---
+++ WindowsFormsApplication6/DiceUI.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yahtzee
+{
+    public partial class DiceUI : UserControl
+    {
+        private DiceController controller;
+
+        private bool keepDice0 = false;
+        private bool keepDice1 = false;
+        private bool keepDice2 = false;
+        private bool keepDice3 = false;
+        private bool keepDice4 = false;
+
+        private int turnToTrow = 3;
+
+        public DiceUI( DiceController controller)
+        {
+            this.controller = controller;
+            InitializeComponent();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (keepDice0 == true)
+            {
+                keepDice0 = false;
+
+                this.button0.BackColor = System.Drawing.Color.White;
+                this.teerlingValue0.BackColor = System.Drawing.Color.White;
+            }
+            else
+            {
+                keepDice0 = true;
+
+                this.button0.BackColor = System.Drawing.Color.Gray;
+                this.teerlingValue0.BackColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (keepDice1 == true)
+            {
+                keepDice1 = false;
+
+                this.button1.BackColor = System.Drawing.Color.White;
+                this.teerlingValue1.BackColor = System.Drawing.Color.White;
+            }
+            else
+            {
+                keepDice1 = true;
+
+                this.button1.BackColor = System.Drawing.Color.Gray;
+                this.teerlingValue1.BackColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (keepDice2 == true)
+            {
+                keepDice2 = false;
+
+                this.button2.BackColor = System.Drawing.Color.White;
+                this.teerlingValue2.BackColor = System.Drawing.Color.White;
+            }
+            else
+            {
+                keepDice2 = true;
+
+                this.button2.BackColor = System.Drawing.Color.Gray;
+                this.teerlingValue2.BackColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (keepDice3 == true)
+            {
+                keepDice3 = false;
+
+                this.button3.BackColor = System.Drawing.Color.White;
+                this.teerlingValue3.BackColor = System.Drawing.Color.White;
+            }
+            else
+            {
+                keepDice3 = true;
+
+                this.button3.BackColor = System.Drawing.Color.Gray;
+                this.teerlingValue3.BackColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (keepDice4 == true)
+            {
+                keepDice4 = false;
+
+                this.button4.BackColor = System.Drawing.Color.White;
+                this.teerlingValue4.BackColor = System.Drawing.Color.White;
+            }
+            else
+            {
+                keepDice4 = true;
+
+                this.button4.BackColor = System.Drawing.Color.Gray;
+                this.teerlingValue4.BackColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void buttonAll_Click(object sender, EventArgs e)
+        {
+            if (turnToTrow > 0)
+            {
+                int newValue = this.controller.throwDice();
+
+                if (keepDice0 == false)
+                {
+                    this.teerlingValue0.Text = newValue.ToString();
+                    newValue = this.controller.throwDice();
+                    this.button0.Visible = true;
+                }
+                else
+                {
+                    this.button0.Visible = false;
+                }
+                if (keepDice1 == false)
+                {
+                    this.teerlingValue1.Text = newValue.ToString();
+                    newValue = this.controller.throwDice();
+                    this.button1.Visible = true;
+                }
+                else
+                {
+                    this.button1.Visible = false;
+                }
+                if (keepDice2 == false)
+                {
+                    this.teerlingValue2.Text = newValue.ToString();
+                    newValue = this.controller.throwDice();
+                    this.button2.Visible = true;
+                }
+                else
+                {
+                    this.button2.Visible = false;
+                }
+                if (keepDice3 == false)
+                {
+                    this.teerlingValue3.Text = newValue.ToString();
+                    newValue = this.controller.throwDice();
+                    this.button3.Visible = true;
+                }
+                else
+                {
+                    this.button3.Visible = false;
+                }
+                if (keepDice4 == false)
+                {
+                    this.teerlingValue4.Text = newValue.ToString();
+                    newValue = this.controller.throwDice();
+                    this.button4.Visible = true;
+                }
+                else
+                {
+                    this.button4.Visible = false;
+                }
+                this.buttonAll.Text = "werp alle overige teerlingen";
+                if (turnToTrow < 2)
+                {
+                    button0.Visible = false;
+                    button1.Visible = false;
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    button4.Visible = false;
+
+                    this.teerlingValue0.BackColor = System.Drawing.Color.Gray;
+                    this.teerlingValue1.BackColor = System.Drawing.Color.Gray;
+                    this.teerlingValue2.BackColor = System.Drawing.Color.Gray;
+                    this.teerlingValue3.BackColor = System.Drawing.Color.Gray;
+                    this.teerlingValue4.BackColor = System.Drawing.Color.Gray;
+                }
+                turnToTrow--;
+            }
+        }
+
+        private void DiceUI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
